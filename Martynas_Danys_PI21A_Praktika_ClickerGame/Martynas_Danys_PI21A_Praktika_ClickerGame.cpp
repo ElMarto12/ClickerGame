d327d73305445e4a27ce83f1870a64a77e1517a6
m10da <m10da@DESKTOP-2KMDUVO>
2022-05-11 15:26:18 +0300

    Add project files.

---
+++ Martynas_Danys_PI21A_Praktika_ClickerGame/Martynas_Danys_PI21A_Praktika_ClickerGame.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class ContentGenerator {
+	public:
+
+		vector<string> Data;
+
+		string FilePath = "GameData.csv";
+
+		void ImportData() {
+
+			ifstream File;
+
+			File.open(FilePath);
+
+			if (File.fail()) {
+
+				cout << "Error !! Game Data not reached " << FilePath << endl;
+				return;
+			}
+
+			// Put all read data to Vector array
+			while (File.peek() != EOF) {
+
+				string record;
+
+				getline(File, record, ',');
+
+				Data.push_back(record);
+
+
+			}
+
+			File.close();			
+
+		}
+
+		int GetData(int select) {
+
+			int value;
+
+			// Conversion from string to integer
+			value = stoi(Data.at(select));
+
+			return value;
+
+
+		}
+
+		void ExportData() { //-------- To update data in Vector, must use ImportData, othervise it still same as originally was
+
+			ofstream File(FilePath, ios::app);
+
+			string xp_number;
+			
+			cout << "Insert amount of xp points to reach another level: "; cin >> xp_number;
+
+			File << xp_number << "," << "\n";
+
+			File.close();
+
+		}
+
+	private:
+
+		void TraverseVector() {
+			
+			// Traverses through all Vector elements, despite data type || !! Data in Vector, at this point, not nessisarily can be integer or any other type !! ||
+			for (auto itr : Data) {
+
+				cout << itr << " ";
+			}
+
+		}
+
+
+};
+
+
+
+
+
+int main() {
+
+	ContentGenerator Gen;
+
+}
+
+
+

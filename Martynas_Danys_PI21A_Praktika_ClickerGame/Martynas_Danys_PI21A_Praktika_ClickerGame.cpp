f9927e13e7be6eec85ffa4d657bad53378ede6ab
Martynas Danys <s043012@ad.viko.lt>
2022-05-20 01:20:38 +0300

    Finish

--- Martynas_Danys_PI21A_Praktika_ClickerGame/Martynas_Danys_PI21A_Praktika_ClickerGame.cpp
+++ Martynas_Danys_PI21A_Praktika_ClickerGame/Martynas_Danys_PI21A_Praktika_ClickerGame.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <windows.h>
 
 using namespace std;
 
@@ -81,7 +80,7 @@ public:
 
 };
 
-class Expierience : ContentGenerator{
+class Expierience : ContentGenerator {
 protected:
 	int experience = 0;
 	int level = 1;
@@ -127,9 +126,7 @@ public:
 
 };
 
-
-
-class Player {
+class Player{
 protected:
 	string name;
 	int score = 0;
@@ -214,9 +211,10 @@ public:
 class Items{
 protected:
 	//Items for score
+	
 	vector<int> item, price;
-	int Score_ItemArray[5] = {100, 50, 30, 20, 15 };
-	int Score_Price[5] = { 2000, 1000, 800, 600, 300};
+	int Score_ItemArray[5] = { 100, 50, 30, 20, 15 };
+	int Score_Price[5] = { 2000, 1000, 800, 600, 300 };
 
 	//Items for xp
 	vector<int> xp_item, xp_price;
@@ -224,7 +222,6 @@ protected:
 	int Xp_Price[5] = { 500, 800, 1000, 1500, 3000 };
 
 public:
-
 	void Score_ImportItems() {
 
 		int items;
@@ -265,7 +262,7 @@ public:
 
 		for (int i = 0; i < item.size(); i++) {
 
-			cout << xp_item[i] << " " << xp_price[i] << endl;
+			cout << "|| " << i << " Additional Xp Points : " << xp_item[i] << " " << "Price : " << xp_price[i] << endl;
 		}
 
 	}
@@ -274,25 +271,48 @@ public:
 		
 		// Print items and price
 		for (int i = 0; i < item.size(); i++) {
-
-			cout << item[i] << " " << price[i] << endl;
+			 
+			cout << "|| " << i << " Increase Rate by : " << item[i] << " " << "Price : " << price[i] << endl;
 
 		}
 
 	}
 
-
-
 };
 
+class Inventory: Items, Player {
+public:
+	vector<int> s_items, s_price;
+	vector<int> xp_items;
+
+	void Score_ItemsList() {
+
+		for (int i = 0; i < s_items.size(); i++) {
+
+			cout << "|| " << i << " Your rate increase Item : " << s_items[i] << " Sell value : " << s_price[i] << endl;
+
+		}
+	}
+
+	void Xp_ItemsList() {
+
+		for (int i = 0; i < xp_items.size(); i++) {
+
+			cout << "|| " << i << " Your Additional XP : " << xp_items[i] << endl;
+
+		}
+	}
+
+};
 
 class Game : Player, Expierience, Items{
 public:
 	bool IsGoing = true;
 
-	int Shop_select;
-	int select;
+	int Shop_select, select, choose, temp, inv_select;
 	string selection;
+
+	Inventory inv;
 
 	bool running() {
 
@@ -321,7 +341,24 @@ public:
 
 	void Instructions() {
 
-		cout << "testing" << endl;
+		cout << "############################################" << endl;
+		cout << "# * The Game is about clicking             #" << endl;
+		cout << "# ---------------------------------------- #" << endl;
+		cout << "# By pressing click, you will gain Points  #" << endl;
+		cout << "#          Also gain experience            #" << endl;
+		cout << "#                                          #" << endl;
+		cout << "#    With Points you can access Shop       #" << endl;
+		cout << "#  Increase Rate to gain points per click  #" << endl;
+		cout << "#   Or buy additional experience points    #" << endl;
+		cout << "#                                          #" << endl;
+		cout << "# In Inventory, you can manage your items  #" << endl;
+		cout << "#              Sell or Use                 #" << endl;
+		cout << "#----------------------------------------- #" << endl;
+		cout << "#       Press Start Game and enjoy         #" << endl;
+		cout << "############################################" << endl;
+		cout << "||  Press B to go back                    ||" << endl;
+		cout << "============================================" << endl;
+		
 
 	}
 
@@ -339,11 +376,10 @@ public:
 
 	void HUD() {
 
-
 		system("cls");
 
-		Score_ImportItems();
 		Xp_ImportItems();
+		Score_ImportItems();
 
 		LootGenerator Obj;
 
@@ -359,9 +395,9 @@ public:
 			//Prints info of got loot
 			Obj.PrintLootInfo();
 
-			cout << "####################################################################################" << endl;
-			cout << "#  1. Click  ||  2. Shop  ||  3. Inventory  ||  4. Achievments  ||  0. Exit Game   #" << endl;
-			cout << "####################################################################################" << endl;
+			cout << "################################################################" << endl;
+			cout << "#  1. Click  ||  2. Shop  ||  3. Inventory  ||  0. Exit Game   #" << endl;
+			cout << "################################################################" << endl;
 
 			cin >> select;
 
@@ -400,6 +436,18 @@ public:
 					Obj.CountIteration_Xp = 0;
 
 				}
+				else if (inv.s_items.empty() == false) {
+
+					//Traverses through inventory items vector, and every found value adds to score
+					for (int i = 0; i < inv.s_items.size(); i++) {
+
+						score = score + inv.s_items[i];
+
+					}
+					experience = experience + 2;
+
+								
+				}
 				else {
 
 					score++;
@@ -416,6 +464,15 @@ public:
 				ShopMenu();
 
 				break;
+			case 3:
+				system("cls");
+					
+				InventoryMenu();
+
+				break;
+			case 0:
+				
+				break;
 			default:
 				break;
 			}
@@ -426,30 +483,563 @@ public:
 
 	void ShopMenu() {
 
-		system("cls");
+		while (true) {
 
-		cout << "#############################" << endl;
-		cout << "#                           #" << endl;
-		cout << "#       Clicky Shop         #" << endl;
-		cout << "#                           #" << endl;
-		cout << "#############################" << endl;
+			system("cls");
 
-		Print_Items_Score();
-		Print_Items_Xp();
+			cout << "###################################" << endl;
+			cout << "#                                 #" << endl;
+			cout << "#           Clicky Shop           #" << endl;
+			cout << "#                                 #" << endl;
+			cout << "###################################" << endl;
+			cout << "===================================" << endl;
+			cout << "|| Points: " << GetScore() << endl;
+			cout << "===================================" << endl;
+			cout << "###################################" << endl;
+			cout << "|| 1. Point Items || 2. Xp Items ||" << endl;
+			cout << "###################################" << endl;
+			cout << "||  Enter any number to go back  ||" << endl;
+			cout << "===================================" << endl;
 
-		cin >> Shop_select;
+			cin >> Shop_select;
+
+			if (Shop_select == 1) {
+
+				Points_Shop();
+
+			}
+
+			if (Shop_select == 2) {
+
+				Xp_Shop();
+
+			}
+
+			break;
+		}
 
 	}
+
+	void Points_Shop() {
+		
+
+		system("cls");
+
+		cout << "###################################" << endl;
+		cout << "#                                 #" << endl;
+		cout << "#           Clicky Shop           #" << endl;
+		cout << "#                                 #" << endl;
+		cout << "###################################" << endl;
+		cout << "===================================" << endl;
+		cout << "|| Points: " << GetScore() << endl;
+		cout << "===================================" << endl;
+		cout << "===================================" << endl;
+		cout << "|| Point Items :                 ||" << endl;
+		cout << "===================================" << endl;
+		Print_Items_Score();
+		cout << "!! Select only Items, which are available !!" << endl;
+
+		cin >> choose;
+
+		if (choose == 0) {
+
+			if (GetScore() >= price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.s_items.push_back(item[choose]);
+				inv.s_price.push_back(price[choose]);
+
+				score = score - price[choose];
+
+				//erases item from shop and also price
+				item.erase(item.begin() + choose);
+				price.erase(price.begin() + choose);
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+			
+
+		}
+
+		if (choose == 1) {
+
+			if (GetScore() >= price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.s_items.push_back(item[choose]);
+				inv.s_price.push_back(price[choose]);
+
+				score = score - price[choose];
+
+				//erases item from shop and also price
+				item.erase(item.begin() + choose);
+				price.erase(price.begin() + choose);
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+		}
+
+		if (choose == 2) {
+
+			if (GetScore() >= price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.s_items.push_back(item[choose]);
+				inv.s_price.push_back(price[choose]);
+
+				score = score - price[choose];
+
+				//erases item from shop and also price
+				item.erase(item.begin() + choose);
+				price.erase(price.begin() + choose);
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+		}
+
+		if (choose == 3) {
+
+			if (GetScore() >= price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.s_items.push_back(item[choose]);
+				inv.s_price.push_back(price[choose]);
+
+				score = score - price[choose];
+
+				//erases item from shop and also price
+				item.erase(item.begin() + choose);
+				price.erase(price.begin() + choose);
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+		}
+		if (choose == 4) {
+
+			if (GetScore() >= price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.s_items.push_back(item[choose]);
+				inv.s_price.push_back(price[choose]);
+
+				score = score - price[choose];
+
+				//erases item from shop and also price
+				item.erase(item.begin() + choose);
+				price.erase(price.begin() + choose);
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+		}
+
+		else {
+
+			ShopMenu();
+		}
+	}
+
+	void Xp_Shop() {
+
+		system("cls");
+
+		cout << "###################################" << endl;
+		cout << "#                                 #" << endl;
+		cout << "#           Clicky Shop           #" << endl;
+		cout << "#                                 #" << endl;
+		cout << "###################################" << endl;
+		cout << "===================================" << endl;
+		cout << "|| Points: " << GetScore() << endl;
+		cout << "===================================" << endl;
+		cout << "===================================" << endl;
+		cout << "|| XP Items :                 ||" << endl;
+		cout << "===================================" << endl;
+		Print_Items_Xp();
+		cout << "!! Select only Items, which are available !!" << endl;
+
+		cin >> choose;
+
+		if (choose == 0) {
+
+			if (GetScore() >= xp_price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.xp_items.push_back(xp_item[choose]);
+			
+				score = score - xp_price[choose];
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+
+		}
+
+		if (choose == 1) {
+
+			if (GetScore() >= xp_price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.xp_items.push_back(xp_item[choose]);
+
+				score = score - xp_price[choose];
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+		}
+
+		if (choose == 2) {
+
+			if (GetScore() >= xp_price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.xp_items.push_back(xp_item[choose]);
+
+				score = score - xp_price[choose];
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+		}
+
+		if (choose == 3) {
+
+			if (GetScore() >= xp_price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.xp_items.push_back(xp_item[choose]);
+
+				score = score - xp_price[choose];
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+		}
+		if (choose == 4) {
+
+			if (GetScore() >= xp_price[choose]) {
+
+				//inserts bought item to inventory vector
+				inv.xp_items.push_back(xp_item[choose]);
+
+				score = score - xp_price[choose];
+
+			}
+			else {
+
+				cout << "Sorry !! You don't have enough Points " << endl;
+				cout << "Enter any number to continue...." << endl;
+				cin >> temp;
+
+			}
+
+		}
+
+		else {
+
+			ShopMenu();
+
+		}
+	}
+
+	void InventoryMenu() {
+
+		while (true) {
+
+			system("cls");
+
+			cout << "#################################" << endl;
+			cout << "#                               #" << endl;
+			cout << "#      Clicked Inventory        #" << endl;
+			cout << "#                               #" << endl;
+			cout << "#################################" << endl;
+			cout << "\n";
+			cout << "#################################" << endl;
+			cout << "|| 1. Check Points Items       ||" << endl;
+			cout << "#################################" << endl;
+			cout << "|| 2. Check Xp Items           ||" << endl;
+			cout << "#################################" << endl;
+			cout << "\n";
+			cout << "=================================" << endl;
+			cout << "|| Enter any number to go back ||" << endl;
+			cout << "=================================" << endl;
+
+			cin >> inv_select;
+
+			if (inv_select == 1) {
+
+				Points_Menu();
+
+			}
+
+			if (inv_select == 2) {
+
+				XP_Menu();
+
+			}
+
+			break;
+		}
+
+	}
+
+	void Points_Menu() {
+
+		system("cls");
+
+		cout << "#################################" << endl;
+		cout << "#                               #" << endl;
+		cout << "#      Clicked Inventory        #" << endl;
+		cout << "#                               #" << endl;
+		cout << "#################################" << endl;
+		cout << "\n";
+		cout << "#################################" << endl;
+		cout << "# Your Points Items:            #" << endl;
+		cout << "#################################" << endl;
+		inv.Score_ItemsList();
+		cout << "---------------------------------" << endl;
+		cout << "|| Select Item To Sell         ||" << endl;
+		cout << "---------------------------------" << endl;
+		cout << "=================================" << endl;
+		cout << "|| Enter any number to go back ||" << endl;
+		cout << "=================================" << endl;
+
+		cin >> inv_select;
+
+		if (inv_select == 0) {
+
+			// Puts back Item and Price to Shop
+			item.push_back(inv.s_items[inv_select]);
+			price.push_back(inv.s_price[inv_select]);
+
+			// Adds back amount of Points to score
+			score = score + inv.s_price[inv_select];
+
+			//Removes Item from inventory
+			inv.s_items.erase(inv.s_items.begin() + inv_select);
+			inv.s_price.erase(inv.s_price.begin() + inv_select);
+
+			cout << "Your Item is Sold !! " << endl;
+
+			cin >> inv_select;
+		}
+
+		if(inv_select == 1) {
+		
+			item.push_back(inv.s_items[inv_select]);
+			price.push_back(inv.s_price[inv_select]);
+
+			score = score + inv.s_price[inv_select];
+
+			inv.s_items.erase(inv.s_items.begin() + inv_select);
+			inv.s_price.erase(inv.s_price.begin() + inv_select);
+
+			cout << "Your Item is Sold !! " << endl;
+
+			cin >> inv_select;
+		}
+
+		if (inv_select == 2) {
+
+			item.push_back(inv.s_items[inv_select]);
+			price.push_back(inv.s_price[inv_select]);
+
+			score = score + inv.s_price[inv_select];
+
+			inv.s_items.erase(inv.s_items.begin() + inv_select);
+			inv.s_price.erase(inv.s_price.begin() + inv_select);
+
+			cout << "Your Item is Sold !! " << endl;
+
+			cin >> inv_select;
+
+		}
+
+		if (inv_select == 3) {
+
+			item.push_back(inv.s_items[inv_select]);
+			price.push_back(inv.s_price[inv_select]);
+
+			score = score + inv.s_price[inv_select];
+
+			inv.s_items.erase(inv.s_items.begin() + inv_select);
+			inv.s_price.erase(inv.s_price.begin() + inv_select);
+
+			cout << "Your Item is Sold !! " << endl;
+
+			cin >> inv_select;
+
+		}
+
+		if (inv_select == 4) {
+
+			item.push_back(inv.s_items[inv_select]);
+			price.push_back(inv.s_price[inv_select]);
+
+			score = score + inv.s_price[inv_select];
+
+			inv.s_items.erase(inv.s_items.begin() + inv_select);
+			inv.s_price.erase(inv.s_price.begin() + inv_select);
+
+			cout << "Your Item is Sold !! " << endl;
+
+			cin >> inv_select;
+
+		}
+
+		InventoryMenu();
+
+	}
+
+	void XP_Menu() {
+
+		system("cls");
+
+		cout << "#################################" << endl;
+		cout << "#                               #" << endl;
+		cout << "#      Clicked Inventory        #" << endl;
+		cout << "#                               #" << endl;
+		cout << "#################################" << endl;
+		cout << "\n";
+		cout << "#################################" << endl;
+		cout << "# Your XP Items:                #" << endl;
+		cout << "#################################" << endl;
+		inv.Xp_ItemsList();
+		cout << "---------------------------------" << endl;
+		cout << "|| Select Item To Activate     ||" << endl;
+		cout << "---------------------------------" << endl;
+		cout << "=================================" << endl;
+		cout << "|| Enter any number to go back ||" << endl;
+		cout << "=================================" << endl;
+
+		cin >> inv_select;
+
+		if (inv_select == 0) {
+
+			//Adds experience after activating xp booster
+			experience = experience + inv.xp_items[inv_select];
+
+			// Removes from inventory
+			inv.xp_items.erase(inv.xp_items.begin() + inv_select);
+
+			cout << "Sucsessfully added experience !! " << endl;
+			cin >> inv_select;
+		}
+
+		if (inv_select == 1) {
+
+			experience = experience + inv.xp_items[inv_select];
+
+			inv.xp_items.erase(inv.xp_items.begin() + inv_select);
+
+			cout << "Sucsessfully added experience !! " << endl;
+			cin >> inv_select;
+
+		}
+
+		if (inv_select == 2) {
+
+			experience = experience + inv.xp_items[inv_select];
+
+			inv.xp_items.erase(inv.xp_items.begin() + inv_select);
+
+			cout << "Sucsessfully added experience !! " << endl;
+			cin >> inv_select;
+
+		}
+
+		if (inv_select == 3) {
+
+			experience = experience + inv.xp_items[inv_select];
+
+			inv.xp_items.erase(inv.xp_items.begin() + inv_select);
+
+			cout << "Sucsessfully added experience !! " << endl;
+			cin >> inv_select;
+
+		}
+
+		if (inv_select == 4) {
+
+			experience = experience + inv.xp_items[inv_select];
+
+			inv.xp_items.erase(inv.xp_items.begin() + inv_select);
+
+			cout << "Sucsessfully added experience !! " << endl;
+			cin >> inv_select;
+
+		}
+
+		InventoryMenu();
+
+	}
+
 };
 
 
 int main() {
 
-
 	Game loop;
 
 	// Game Loop
 	while (loop.running()) {
+
+		system("cls");
 
 		loop.MainMenu();
 		
